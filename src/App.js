4ac861ceda8fc252170df5ea1904281f19dd0247
VictorWinberg <dat14vwi@student.lu.se>
2017-04-24 14:42:23 +0200

    likes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Hello /> with <Name first="Victor" last="Winberg" />
+        <Hello />
+        <p>
+          <Name first="Victor" last="Winberg " />
+          <Likes amount="1337" />
+        </p>
+        <p>
+          <Name first="Anton" last="Göransson " />
+          <Likes amount="-3" />
+        </p>
       </div>
     )
   }
@@ -17,6 +25,25 @@ const Hello = () => <span>Hello World</span>;
 class Name extends React.Component {
   render() {
     return <span>{ this.props.first } { this.props.last }</span>;
+  }
+}
+
+class Likes extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { likes: 0, amount: parseInt(props.amount) }
+    this.updateLikes = this.updateLikes.bind(this);
+  }
+
+  updateLikes() {
+    this.setState({ likes: this.state.likes + this.state.amount });
+  }
+
+  render() {
+    return <span>
+      likes: { this.state.likes }
+      <button onClick={ this.updateLikes }>Like</button>
+    </span>
   }
 }
 
